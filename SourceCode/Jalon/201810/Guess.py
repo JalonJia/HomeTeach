1be41190c6968ed064a7a78b919a1b774371b581
JalonJia <jalon.jia@hotmail.com>
2018-10-06 17:25:38 +0800

    提交修改的猜数字游戏

--- SourceCode/Jalon/201810/Guess.py
+++ SourceCode/Jalon/201810/Guess.py
@@ -2,49 +2,33 @@
 
 import random
 
-number = random.randint(1, 10) #产生一个1到10之间的随机整数
+number = random.randint(1, 50) #产生一个1到10之间的随机整数
 
 #print(number)
 
-print('猜数字，猜一个1到10之间的数字，如果6次以内你猜对了，你就赢了；否则，你就输了')
-guess = int(input('请输入你猜测的数字：'))
-if guess == number :
-    print('恭喜你，你猜对了！')
-    quit()
-else:
-    print('就差一点点了，继续猜吧！')
-    
-guess = int(input('请输入你猜测的数字'))
-if guess == number :
-    print('恭喜你，你猜对了！')
-    quit()
-else:
-    print('就差一点点了，继续猜吧！')
+print('猜数字，猜一个1到50之间的数字，如果6次以内你猜对了，你就赢了；否则，你就输了')
 
-guess = int(input('请输入你猜测的数字'))
-if guess == number :
-    print('恭喜你，你猜对了！')
-    quit()
-else:
-    print('就差一点点了，继续猜吧！')
-
-guess = int(input('请输入你猜测的数字'))
-if guess == number :
-    print('恭喜你，你猜对了！')
-    quit()
-else:
-    print('就差一点点了，继续猜吧！')
-
-guess = int(input('请输入你猜测的数字'))
-if guess == number :
-    print('恭喜你，你猜对了！')
-    quit()
-else:
-    print('就差一点点了，继续猜吧！')
-
-guess = int(input('请输入你猜测的数字'))
-if guess == number :
-    print('恭喜你，你猜对了！')
-    quit()
-else:
-    print('就差一点点了，继续猜吧！')
+for i in range(6): #for循环，i:自增变量，range(6):迭代器
+    print(f'第几次循环：{i}')
+    get_enter = input('请输入你猜测的数字：')
+    if get_enter.isdigit() : #判断输入的是不是数字
+        guess = int(get_enter) #将参数转换成数字
+    else:
+        print('你输入的不是一个数字，请重新输入')        
+        continue
+        
+    if guess == number: #猜对的情况
+        print('恭喜你，你猜对了！')
+        break
+    elif guess > number:  #猜错的情况，其实有两种：猜的数比答案大或者比答案小,这是第一种
+        if i == 5:
+            print(f'很抱歉，你没有猜出来，你输了！答案是：{number}')
+        else:
+            print('你猜的数字大了，继续猜吧！')
+    else:
+        if i == 5:
+            print(f'很抱歉，你没有猜出来，你输了！答案是：{number}')
+        else:
+            print('你猜的数字小了，继续猜吧！')
+        
+        
