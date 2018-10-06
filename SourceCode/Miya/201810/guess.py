b4962598874d08f476b3bd15b711d5aa887d6431
jalon.jia <jalon.jia@norming.com.cn>
2018-10-06 13:12:49 +0800

    提交代码

---
+++ SourceCode/Miya/201810/guess.py
@@ -0,0 +1,52 @@
+import random
+number=random.randint(1, 10)
+print('猜一猜 数字')
+caicai=int(input ())
+if caicai==number:
+      print('恭喜你，你猜对了！')
+      quit()
+else:      
+      print('就差一点点了，继续猜吧！')           
+
+caicai=int(input ())
+if caicai==number:
+      print('恭喜你，你猜对了！')
+      quit()
+else:      
+      print('就差一点点了，继续猜吧！')           
+
+caicai=int(input ())
+if caicai==number:
+      print('恭喜你，你猜对了！')
+      quit()
+else:      
+      print('就差一点点了，继续猜吧！')           
+
+caicai=int(input ())
+if caicai==number:
+      print('恭喜你，你猜对了！')
+      quit()
+else:      
+      print('就差一点点了，继续猜吧！')           
+
+caicai=int(input ())
+if caicai==number:
+      print('恭喜你，你猜对了！')
+      quit()
+else:      
+      print('就差一点点了，继续猜吧！')           
+
+caicai=int(input ())
+if caicai==number:
+      print('恭喜你，你猜对了！')
+      quit()
+else:      
+      print('就差一点点了，继续猜吧！')           
+
+caicai=int(input ())
+if caicai==number:
+      print('恭喜你，你猜对了！')
+      quit()
+else:      
+      print('就差一点点了，继续猜吧！')           
+
