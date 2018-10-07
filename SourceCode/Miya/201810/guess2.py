0aab1b2f212a9815844903e5f7ed9360f7226cd5
JalonJia <jalon.jia@hotmail.com>
2018-10-07 20:46:53 +0800

    提交Miya的代码

---
+++ SourceCode/Miya/201810/guess2.py
@@ -0,0 +1,7 @@
+print('你几岁了')
+my_age = input()
+       
+if my_age>10:
+    print('你' +my_age +'岁了 你需要买票')       
+else:
+    print('你小于10岁 你不用买票')                                                        
