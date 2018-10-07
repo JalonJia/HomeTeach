34ac658ba0e81d800a9785100889ccd32cd8785d
jalon.jia <jalon.jia@norming.com.cn>
2018-10-07 17:44:15 +0800

    提交代码Kevin

--- SourceCode/Kevin/201810/guess.py
+++ SourceCode/Kevin/201810/guess.py
@@ -1,13 +1,25 @@
+g = 0
+w = 1
+q = 1
+print('猜数字在1—100里面,猜对才结束')
 import random
 jkl = random.randint(1,100)
-print('猜猜我爸爸的岁数，是在1—100之间的数,一共有六次机会：')
-for i in range(6):
-    iop = int(input())
-    if iop == jkl:
-        print('你猜对了!')
-        quit()
-    if iop < jkl:
+while q == w:
+    iop = input()
+    while q == w:
+            if not iop.isdigit():
+                print('你输入的不是数字请重新输入')
+                iop = input()
+                g += 1
+            else:
+                g += 1
+                break
+    if int(iop) == jkl:
+        print(f'你猜对了!,你一共猜了{g}次')
+        break
+    if int(iop) < jkl:
         print('你猜的数小了。')
-    if iop > jkl:
+        print('你应该猜大')
+    if int(iop) > jkl:
         print('你猜的数大了。')
-print('你输了。')
+        print('你应该猜小')
