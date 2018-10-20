645f82bb7f6bcb077d30248bc5ff4e321b1be0e2
JalonJia <jalon.jia@hotmail.com>
2018-10-20 18:28:17 +0800

    提交讲课代码

---
+++ SourceCode/Jalon/201810/draw.py
@@ -0,0 +1,39 @@
+import turtle #导入乌龟画图模块
+
+pen = turtle.Turtle() #初始化一个画布，并生成画笔，画笔变量名为pen
+
+#正方形
+pen.fd(100) #向前
+pen.lt(90) #拐弯
+pen.fd(100) 
+pen.lt(90) #拐弯
+pen.fd(100) 
+pen.lt(90) #拐弯
+pen.fd(100)
+pen.lt(90) #拐弯
+
+
+
+#清楚屏幕
+pen.clear()
+
+#长方形
+pen.fd(200) #向前
+pen.lt(90) #拐弯
+pen.fd(100) 
+pen.lt(90) #拐弯
+pen.fd(200) 
+pen.lt(90) #拐弯
+pen.fd(100)
+
+pen.clear()
+
+
+#用for循环画正方形
+for i in range(4):
+    pen.fd(100)
+    pen.lt(90)
+
+
+
+turtle.mainloop()
